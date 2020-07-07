5dd76a3c2745c15c39fd69ce6500ab53c351f105
Eduardo Dorneles <eduardo.dorneles.fs@gmail.com>
2020-07-07 19:04:19 -0300

    I wasn't capable of solving a problem with H2 database in 'test' context, so I decided to restart the entire project from zero to fix it and to re-learn everything.

--- src/main/java/com/lyrnics/enums/UserRoleEnum.java
+++
@@ -1,7 +0,0 @@
-package com.lyrnics.enums;
-
-public enum UserRoleEnum {
-    //It is necessary to keep the 'ROLE' prefix to make the spring security understand it
-    ROLE_COMMON, 
-    ROLE_ADMIN
-}//END Enum
